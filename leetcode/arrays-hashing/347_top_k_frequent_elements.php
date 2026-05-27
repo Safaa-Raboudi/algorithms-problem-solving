eaca50e559a04760fd4e3534d6f6f6d1d29e0c4c
safa-rbd <raboudi25.safa@gmail.com>
2026-05-27 21:52:04 +0200

    347_top_k_frequent_elements

---
+++ leetcode/arrays-hashing/347_top_k_frequent_elements.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+Problem: Top K Frequent Elements
+
+Link: https://leetcode.com/problems/top-k-frequent-elements/
+*/
+
+class Solution
+{
+    /**
+     * @param array $nums
+     * @param int $k
+     * 
+     * @return array
+     */
+    public function topKFrequent(array $nums, int $k): array
+    {
+        $freq = [];
+
+        foreach ($nums as $num) {
+            $freq[$num] = ($freq[$num] ?? 0) + 1;
+        }
+
+        arsort($freq);
+
+        return array_slice(array_keys($freq), 0, $k);
+    }
+}
---
+++ leetcode/arrays-hashing/347_top_k_frequent_elements.py
@@ -0,0 +1,20 @@
+"""
+Problem: Top K Frequent Elements
+
+Link: https://leetcode.com/problems/top-k-frequent-elements/
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        sorted_nums = sorted(freq, key=freq.get, reverse=True)
+
+        return sorted_nums[:k]
